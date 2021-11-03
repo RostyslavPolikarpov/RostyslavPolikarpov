339e80165c4291fba69c7b02c6a14bb2279bc0e6
Rostyslav Polikarpov <polikarpov.r.a13@gmail.com>
2021-11-03 20:13:47 +0200

    corrected homework

--- src/main/java/ua/polikarpov/hw7/Task3.java
+++ src/main/java/ua/polikarpov/hw7/Task3.java
@@ -4,30 +4,6 @@ import java.util.Arrays;
 
 import java.util.Random;
 
-public class Task3 {
-    public static void main(String[] args) {
-        Vector vector1 = new Vector(5, 7, -1);
-        System.out.println("Length of vector1: " + vector1.vectorLength());
-        Vector vector2 = new Vector(7, 2, 9);
-        System.out.println("Length of vector2: " + vector2.vectorLength());
-        System.out.println("Cosine between vectors: " + vector1.cosineBetweenVectors(vector2));
-        System.out.println("Sum of vectors: " + vector1.sumOfVectors(vector2));
-        System.out.println("Subtraction of vectors: " + vector1.subtractionOfVectors(vector2));
-        System.out.println("Product of vectors: " + vector1.productOfVectors(vector2));
-        System.out.println(Arrays.toString(getRandomVectors(5)));
-    }
-
-    // метод, принимающий число N и возвращающий массив случайных векторов
-    public static Vector[] getRandomVectors(int size) {
-        Vector[] vectors = new Vector[size];
-        Random random = new Random();
-        for (int i = 0; i < vectors.length; i++) {
-            vectors[i] = new Vector(random.nextInt(5), random.nextInt(5), random.nextInt(5));
-        }
-        return vectors;
-    }
-}
-
 record Vector(double x, double y, double z) {
 
     // длина вектора
